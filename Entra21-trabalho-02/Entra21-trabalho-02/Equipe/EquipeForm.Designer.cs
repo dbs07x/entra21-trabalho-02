c2cda36bcce1adc7e2e2b3c7c1d61fdb8afa23f3
Gregory Viegas Zimmer <gregory.viegas.zimmer@hotmail.com>
2022-06-25 16:54:51 -0300

    Equipe Atualizado o equipe forms

--- Entra21-trabalho-02/Entra21-trabalho-02/Equipe/EquipeForm.Designer.cs
+++ Entra21-trabalho-02/Entra21-trabalho-02/Equipe/EquipeForm.Designer.cs
@@ -43,15 +43,46 @@
             this.labelDataProximaMissao = new System.Windows.Forms.Label();
             this.dateTimePickerDataFormacao = new System.Windows.Forms.DateTimePicker();
             this.maskedTextBoxDataProximaMissao = new System.Windows.Forms.MaskedTextBox();
+            this.labelSituacao = new System.Windows.Forms.Label();
+            this.checkBoxEmMissao = new System.Windows.Forms.CheckBox();
+            this.checkBoxNaAldeia = new System.Windows.Forms.CheckBox();
+            this.checkBoxTemporariamenteInativa = new System.Windows.Forms.CheckBox();
+            this.buttonCadastrar = new System.Windows.Forms.Button();
+            this.buttonEditar = new System.Windows.Forms.Button();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.Apagar = new System.Windows.Forms.Button();
+            this.ColumnCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNomeEquipe = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnLider = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnMembro1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnMembro2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnMembro3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnDataFormacao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnDataProximaMissao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnSituacao = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(530, 83);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnCodigo,
+            this.ColumnNomeEquipe,
+            this.ColumnLider,
+            this.ColumnMembro1,
+            this.ColumnMembro2,
+            this.ColumnMembro3,
+            this.ColumnDataFormacao,
+            this.ColumnDataProximaMissao,
+            this.ColumnSituacao});
+            this.dataGridView1.Location = new System.Drawing.Point(339, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(178, 165);
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(839, 407);
             this.dataGridView1.TabIndex = 0;
             // 
             // labelNomeEquipe
@@ -173,11 +204,149 @@
             this.maskedTextBoxDataProximaMissao.TabIndex = 14;
             this.maskedTextBoxDataProximaMissao.ValidatingType = typeof(System.DateTime);
             // 
+            // labelSituacao
+            // 
+            this.labelSituacao.AutoSize = true;
+            this.labelSituacao.Location = new System.Drawing.Point(12, 258);
+            this.labelSituacao.Name = "labelSituacao";
+            this.labelSituacao.Size = new System.Drawing.Size(52, 15);
+            this.labelSituacao.TabIndex = 15;
+            this.labelSituacao.Text = "Situação";
+            // 
+            // checkBoxEmMissao
+            // 
+            this.checkBoxEmMissao.AutoSize = true;
+            this.checkBoxEmMissao.Location = new System.Drawing.Point(12, 285);
+            this.checkBoxEmMissao.Name = "checkBoxEmMissao";
+            this.checkBoxEmMissao.Size = new System.Drawing.Size(83, 19);
+            this.checkBoxEmMissao.TabIndex = 16;
+            this.checkBoxEmMissao.Text = "Em missão";
+            this.checkBoxEmMissao.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxNaAldeia
+            // 
+            this.checkBoxNaAldeia.AutoSize = true;
+            this.checkBoxNaAldeia.Location = new System.Drawing.Point(97, 285);
+            this.checkBoxNaAldeia.Name = "checkBoxNaAldeia";
+            this.checkBoxNaAldeia.Size = new System.Drawing.Size(77, 19);
+            this.checkBoxNaAldeia.TabIndex = 17;
+            this.checkBoxNaAldeia.Text = "Na Aldeia";
+            this.checkBoxNaAldeia.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxTemporariamenteInativa
+            // 
+            this.checkBoxTemporariamenteInativa.AutoSize = true;
+            this.checkBoxTemporariamenteInativa.Location = new System.Drawing.Point(176, 285);
+            this.checkBoxTemporariamenteInativa.Name = "checkBoxTemporariamenteInativa";
+            this.checkBoxTemporariamenteInativa.Size = new System.Drawing.Size(157, 19);
+            this.checkBoxTemporariamenteInativa.TabIndex = 18;
+            this.checkBoxTemporariamenteInativa.Text = "Temporariamente Inativa";
+            this.checkBoxTemporariamenteInativa.UseVisualStyleBackColor = true;
+            // 
+            // buttonCadastrar
+            // 
+            this.buttonCadastrar.Location = new System.Drawing.Point(12, 333);
+            this.buttonCadastrar.Name = "buttonCadastrar";
+            this.buttonCadastrar.Size = new System.Drawing.Size(101, 40);
+            this.buttonCadastrar.TabIndex = 19;
+            this.buttonCadastrar.Text = "Cadastrar";
+            this.buttonCadastrar.UseVisualStyleBackColor = true;
+            // 
+            // buttonEditar
+            // 
+            this.buttonEditar.Location = new System.Drawing.Point(139, 333);
+            this.buttonEditar.Name = "buttonEditar";
+            this.buttonEditar.Size = new System.Drawing.Size(101, 40);
+            this.buttonEditar.TabIndex = 20;
+            this.buttonEditar.Text = "Editar";
+            this.buttonEditar.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Location = new System.Drawing.Point(139, 379);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(101, 40);
+            this.buttonCancelar.TabIndex = 22;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            // 
+            // Apagar
+            // 
+            this.Apagar.Location = new System.Drawing.Point(12, 379);
+            this.Apagar.Name = "Apagar";
+            this.Apagar.Size = new System.Drawing.Size(101, 40);
+            this.Apagar.TabIndex = 21;
+            this.Apagar.Text = "Apagar";
+            this.Apagar.UseVisualStyleBackColor = true;
+            // 
+            // ColumnCodigo
+            // 
+            this.ColumnCodigo.HeaderText = "Código";
+            this.ColumnCodigo.Name = "ColumnCodigo";
+            this.ColumnCodigo.ReadOnly = true;
+            this.ColumnCodigo.Visible = false;
+            // 
+            // ColumnNomeEquipe
+            // 
+            this.ColumnNomeEquipe.HeaderText = "Nome";
+            this.ColumnNomeEquipe.Name = "ColumnNomeEquipe";
+            this.ColumnNomeEquipe.ReadOnly = true;
+            // 
+            // ColumnLider
+            // 
+            this.ColumnLider.HeaderText = "Líder";
+            this.ColumnLider.Name = "ColumnLider";
+            this.ColumnLider.ReadOnly = true;
+            // 
+            // ColumnMembro1
+            // 
+            this.ColumnMembro1.HeaderText = "Membro 1";
+            this.ColumnMembro1.Name = "ColumnMembro1";
+            this.ColumnMembro1.ReadOnly = true;
+            // 
+            // ColumnMembro2
+            // 
+            this.ColumnMembro2.HeaderText = "Membro2";
+            this.ColumnMembro2.Name = "ColumnMembro2";
+            this.ColumnMembro2.ReadOnly = true;
+            // 
+            // ColumnMembro3
+            // 
+            this.ColumnMembro3.HeaderText = "Membro 3";
+            this.ColumnMembro3.Name = "ColumnMembro3";
+            this.ColumnMembro3.ReadOnly = true;
+            // 
+            // ColumnDataFormacao
+            // 
+            this.ColumnDataFormacao.HeaderText = "Data formação";
+            this.ColumnDataFormacao.Name = "ColumnDataFormacao";
+            this.ColumnDataFormacao.ReadOnly = true;
+            // 
+            // ColumnDataProximaMissao
+            // 
+            this.ColumnDataProximaMissao.HeaderText = "Data próxima missão";
+            this.ColumnDataProximaMissao.Name = "ColumnDataProximaMissao";
+            this.ColumnDataProximaMissao.ReadOnly = true;
+            // 
+            // ColumnSituacao
+            // 
+            this.ColumnSituacao.HeaderText = "Situação";
+            this.ColumnSituacao.Name = "ColumnSituacao";
+            this.ColumnSituacao.ReadOnly = true;
+            // 
             // EquipeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1185, 425);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.Apagar);
+            this.Controls.Add(this.buttonEditar);
+            this.Controls.Add(this.buttonCadastrar);
+            this.Controls.Add(this.checkBoxTemporariamenteInativa);
+            this.Controls.Add(this.checkBoxNaAldeia);
+            this.Controls.Add(this.checkBoxEmMissao);
+            this.Controls.Add(this.labelSituacao);
             this.Controls.Add(this.maskedTextBoxDataProximaMissao);
             this.Controls.Add(this.dateTimePickerDataFormacao);
             this.Controls.Add(this.labelDataProximaMissao);
@@ -194,6 +363,7 @@
             this.Controls.Add(this.labelNomeEquipe);
             this.Controls.Add(this.dataGridView1);
             this.Name = "EquipeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Equipe";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
@@ -218,5 +388,22 @@
         private Label labelDataProximaMissao;
         private DateTimePicker dateTimePickerDataFormacao;
         private MaskedTextBox maskedTextBoxDataProximaMissao;
+        private Label labelSituacao;
+        private CheckBox checkBoxEmMissao;
+        private CheckBox checkBoxNaAldeia;
+        private CheckBox checkBoxTemporariamenteInativa;
+        private DataGridViewTextBoxColumn ColumnCodigo;
+        private DataGridViewTextBoxColumn ColumnNomeEquipe;
+        private DataGridViewTextBoxColumn ColumnLider;
+        private DataGridViewTextBoxColumn ColumnMembro1;
+        private DataGridViewTextBoxColumn ColumnMembro2;
+        private DataGridViewTextBoxColumn ColumnMembro3;
+        private DataGridViewTextBoxColumn ColumnDataFormacao;
+        private DataGridViewTextBoxColumn ColumnDataProximaMissao;
+        private DataGridViewTextBoxColumn ColumnSituacao;
+        private Button buttonCadastrar;
+        private Button buttonEditar;
+        private Button buttonCancelar;
+        private Button Apagar;
     }
 }
