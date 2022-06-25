b8f94bbce0545e7ac08d0781f62814c4927d41ad
dbs07x <dbs07x@gmail.com>
2022-06-25 18:03:37 -0300

    Criação classe ninja

---
+++ Entra21-trabalho-02/Entra21-trabalho-02/Ninja/Ninja.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21_trabalho_02.Ninja
+{
+    internal class Ninja
+    {
+        public int Id;
+        public string Nome;
+        // public Chakra ElementoChakra;
+        public DateTime DataNascimento;
+        // public Genero Genero;
+        // public Status Status;
+        // public Nivel Nivel;
+        // public Cla Cla;
+        // public Aldeia Aldeia
+        public bool KekkeiGenkai;
+
+        /*
+        public bool PossuiEquipe()
+        {
+        return true;
+        }
+        */
+
+        public int CalcularIdade()
+        {
+            var idade = DateTime.Now.Year - DataNascimento.Year;
+
+            return idade;
+        }
+    }
+}
