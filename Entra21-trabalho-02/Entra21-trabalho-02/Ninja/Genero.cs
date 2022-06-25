34fee10414d19abde21b5743578ce45a08a4c78a
dbs07x <dbs07x@gmail.com>
2022-06-25 18:09:20 -0300

    Criação classe genero

---
+++ Entra21-trabalho-02/Entra21-trabalho-02/Ninja/Genero.cs
@@ -0,0 +1,11 @@
+﻿namespace Entra21_trabalho_02.Ninja
+{
+    internal class Genero
+    {
+        public string Masculino;
+        public string Feminino;
+        public string NaoBinario;
+        public string Cisgenero;
+        public string Transgenero;
+    }
+}
