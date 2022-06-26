d22c4e22c931f763ffa128d64508f2ae808cf0c0
claudio raimundo <claudio.h.raimundo123@gmail.com>
2022-06-26 11:21:49 -0300

    Criação e inicio classe AldeiaForm[Design]

---
+++ Entra21-trabalho-02/Entra21-trabalho-02/Aldeia/AldeiaForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Entra21_trabalho_02.Aldeia
+{
+    partial class AldeiaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.buttonEditar = new System.Windows.Forms.Button();
+            this.buttonApagar = new System.Windows.Forms.Button();
+            this.labelNome = new System.Windows.Forms.Label();
+            this.labelPais = new System.Windows.Forms.Label();
+            this.labelLider = new System.Windows.Forms.Label();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.textBoxPais = new System.Windows.Forms.TextBox();
+            this.comboBoxLider = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.Location = new System.Drawing.Point(915, 560);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(135, 64);
+            this.buttonSalvar.TabIndex = 0;
+            this.buttonSalvar.Text = "Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
+            // 
+            // buttonEditar
+            // 
+            this.buttonEditar.Location = new System.Drawing.Point(12, 12);
+            this.buttonEditar.Name = "buttonEditar";
+            this.buttonEditar.Size = new System.Drawing.Size(75, 23);
+            this.buttonEditar.TabIndex = 1;
+            this.buttonEditar.Text = "Editar";
+            this.buttonEditar.UseVisualStyleBackColor = true;
+            // 
+            // buttonApagar
+            // 
+            this.buttonApagar.Location = new System.Drawing.Point(93, 12);
+            this.buttonApagar.Name = "buttonApagar";
+            this.buttonApagar.Size = new System.Drawing.Size(75, 23);
+            this.buttonApagar.TabIndex = 2;
+            this.buttonApagar.Text = "Apagar";
+            this.buttonApagar.UseVisualStyleBackColor = true;
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(734, 46);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(40, 15);
+            this.labelNome.TabIndex = 3;
+            this.labelNome.Text = "Nome";
+            // 
+            // labelPais
+            // 
+            this.labelPais.AutoSize = true;
+            this.labelPais.Location = new System.Drawing.Point(734, 90);
+            this.labelPais.Name = "labelPais";
+            this.labelPais.Size = new System.Drawing.Size(28, 15);
+            this.labelPais.TabIndex = 4;
+            this.labelPais.Text = "País";
+            // 
+            // labelLider
+            // 
+            this.labelLider.AutoSize = true;
+            this.labelLider.Location = new System.Drawing.Point(734, 134);
+            this.labelLider.Name = "labelLider";
+            this.labelLider.Size = new System.Drawing.Size(33, 15);
+            this.labelLider.TabIndex = 5;
+            this.labelLider.Text = "Lider";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(734, 64);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(316, 23);
+            this.textBoxNome.TabIndex = 6;
+            // 
+            // textBoxPais
+            // 
+            this.textBoxPais.Location = new System.Drawing.Point(734, 108);
+            this.textBoxPais.Name = "textBoxPais";
+            this.textBoxPais.Size = new System.Drawing.Size(316, 23);
+            this.textBoxPais.TabIndex = 7;
+            // 
+            // comboBoxLider
+            // 
+            this.comboBoxLider.FormattingEnabled = true;
+            this.comboBoxLider.Location = new System.Drawing.Point(734, 152);
+            this.comboBoxLider.Name = "comboBoxLider";
+            this.comboBoxLider.Size = new System.Drawing.Size(316, 23);
+            this.comboBoxLider.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(701, 578);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // AldeiaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1062, 636);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBoxLider);
+            this.Controls.Add(this.textBoxPais);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.labelLider);
+            this.Controls.Add(this.labelPais);
+            this.Controls.Add(this.labelNome);
+            this.Controls.Add(this.buttonApagar);
+            this.Controls.Add(this.buttonEditar);
+            this.Controls.Add(this.buttonSalvar);
+            this.Name = "AldeiaForm";
+            this.Text = "AldeiaForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button buttonSalvar;
+        private Button buttonEditar;
+        private Button buttonApagar;
+        private Label labelNome;
+        private Label labelPais;
+        private Label labelLider;
+        private TextBox textBoxNome;
+        private TextBox textBoxPais;
+        private ComboBox comboBoxLider;
+        private DataGridView dataGridView1;
+    }
+}
