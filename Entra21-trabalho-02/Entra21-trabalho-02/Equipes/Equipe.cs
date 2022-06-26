d4de117b1a01ec1765fcb40d76366af03095e0d7
Gregory Viegas Zimmer <gregory.viegas.zimmer@hotmail.com>
2022-06-26 11:45:08 -0300

    Equipe Servico Foi criada a classe EquipeServico com o CRUD e demais métodos necessários para realizar os mesmos.

--- Entra21-trabalho-02/Entra21-trabalho-02/Equipes/Equipe.cs
+++ Entra21-trabalho-02/Entra21-trabalho-02/Equipes/Equipe.cs
@@ -1,13 +1,15 @@
-﻿namespace Entra21_trabalho_02.Equipes
+﻿using Entra21_trabalho_02.Ninjas;
+
+namespace Entra21_trabalho_02.Equipes
 {
     internal class Equipe
     {
         public int Id;
         public string Nome;
-        public string Lider;
-        public string Membro1;
-        public string Membro2;
-        public string Membro3;
+        public Ninja Lider;
+        public Ninja Membro1;
+        public Ninja Membro2;
+        public Ninja Membro3;
         public DateTime Formacao;
         public DateTime InicioProximaMissao;
         public Situacao Situacao;
---
+++ Entra21-trabalho-02/Entra21-trabalho-02/Equipes/EquipeServico.cs
@@ -0,0 +1,115 @@
+﻿using Newtonsoft.Json;
+
+namespace Entra21_trabalho_02.Equipes
+{
+    internal class EquipeServico
+    {
+        private List<Equipe> equipes;
+
+        public EquipeServico()
+        {
+            equipes = new List<Equipe>();
+
+            LerArquivo();
+        }
+
+        public void Adicionar(Equipe equipe)
+        {
+            equipes.Add(equipe);
+
+            SalvarArquivo();
+        }
+
+        public void Apagar(Equipe equipeParaApagar)
+        {
+            for (var i = 0; i < equipes.Count; i++)
+            {
+                var equipe = equipes[i];
+
+                if (equipe.Id == equipeParaApagar.Id)
+                {
+                    equipes.Remove(equipe);
+
+                    SalvarArquivo();
+
+                    return;
+                }
+            }
+        }
+
+        public void Editar(Equipe equipeParaAlterar)
+        {
+            for (var i = 0; i < equipes.Count; i++)
+            {
+                var equipe = equipes[i];
+
+                if (equipe.Id == equipeParaAlterar.Id)
+                {
+                    equipe.Nome = equipeParaAlterar.Nome;
+                    equipe.Lider = equipeParaAlterar.Lider;
+                    equipe.Membro1 = equipeParaAlterar.Membro1;
+                    equipe.Membro2 = equipeParaAlterar.Membro2;
+                    equipe.Membro3 = equipeParaAlterar.Membro3;
+                    equipe.Formacao = equipeParaAlterar.Formacao;
+                    equipe.InicioProximaMissao = equipeParaAlterar.InicioProximaMissao;
+                    equipe.Situacao = equipeParaAlterar.Situacao;
+
+                    SalvarArquivo();
+
+                    return;
+                }
+            }
+        }
+
+        public void LerArquivo()
+        {
+            if (File.Exists("equipes.json") == false)
+            {
+                return;
+            }
+
+            var equipesJson = File.ReadAllText("equipes.json");
+            equipes = JsonConvert.DeserializeObject<List<Equipe>>(equipesJson);
+        }
+
+        public void SalvarArquivo()
+        {
+            var equipesJson = JsonConvert.SerializeObject(equipes);
+            File.WriteAllText("equipes.json", equipesJson);
+        }
+
+        public List<Equipe> ListarTodos()
+        {
+            return equipes;
+        }
+
+        public Equipe ObterPorId(int id)
+        {
+            for (var i = 0; i < equipes.Count; i++)
+            {
+                var equipe = equipes[i];
+
+                if (equipe.Id == id)
+                {
+                    return equipe;
+                }
+            }
+
+            return null;
+        }
+
+        public int ObterUltimoId()
+        {
+            var ultimoId = 0;
+
+            for (var i = 0; i < equipes.Count; i++)
+            {
+                var equipe = equipes[i];
+
+                ultimoId = equipe.Id;
+            }
+
+            return ultimoId;
+        }
+    }
+}
