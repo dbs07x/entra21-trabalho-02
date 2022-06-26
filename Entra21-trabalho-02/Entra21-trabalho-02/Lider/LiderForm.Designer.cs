d5b2db3e965d088200701199096da2a9ba57927e
claudio raimundo <claudio.h.raimundo123@gmail.com>
2022-06-26 11:04:20 -0300

    Criação do LiderForm[Design]

--- Entra21-trabalho-02/Entra21-trabalho-02/Lider/LiderForm.Designer.cs
+++ Entra21-trabalho-02/Entra21-trabalho-02/Lider/LiderForm.Designer.cs
@@ -29,17 +29,23 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.buttonEditar = new System.Windows.Forms.Button();
+            this.buttonApagar = new System.Windows.Forms.Button();
+            this.labelNome = new System.Windows.Forms.Label();
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.labelIdade = new System.Windows.Forms.Label();
+            this.labelGenero = new System.Windows.Forms.Label();
+            this.labelInicioLideranca = new System.Windows.Forms.Label();
+            this.labelFimLideranca = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.labelElementoChakra = new System.Windows.Forms.Label();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.comboBoxTitulo = new System.Windows.Forms.ComboBox();
+            this.textBoxIdade = new System.Windows.Forms.TextBox();
+            this.comboBoxGenero = new System.Windows.Forms.ComboBox();
+            this.dateTimePickerInicioLideranca = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerFimLideranca = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,127 +54,177 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 198);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 47);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(595, 578);
             this.dataGridView1.TabIndex = 0;
             // 
-            // button1
+            // buttonSalvar
             // 
-            this.button1.Location = new System.Drawing.Point(723, 494);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonSalvar.Location = new System.Drawing.Point(821, 554);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(120, 71);
+            this.buttonSalvar.TabIndex = 1;
+            this.buttonSalvar.Text = "Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // buttonEditar
             // 
-            this.button2.Location = new System.Drawing.Point(20, 62);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonEditar.Location = new System.Drawing.Point(12, 12);
+            this.buttonEditar.Name = "buttonEditar";
+            this.buttonEditar.Size = new System.Drawing.Size(75, 23);
+            this.buttonEditar.TabIndex = 2;
+            this.buttonEditar.Text = "Editar";
+            this.buttonEditar.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // buttonApagar
             // 
-            this.button3.Location = new System.Drawing.Point(117, 67);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.buttonApagar.Location = new System.Drawing.Point(93, 12);
+            this.buttonApagar.Name = "buttonApagar";
+            this.buttonApagar.Size = new System.Drawing.Size(75, 23);
+            this.buttonApagar.TabIndex = 3;
+            this.buttonApagar.Text = "Apagar";
+            this.buttonApagar.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // labelNome
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(568, 63);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(639, 47);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(40, 15);
+            this.labelNome.TabIndex = 4;
+            this.labelNome.Text = "Nome";
             // 
-            // label2
+            // labelTitulo
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(575, 125);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 15);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.Location = new System.Drawing.Point(639, 91);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(37, 15);
+            this.labelTitulo.TabIndex = 5;
+            this.labelTitulo.Text = "Titulo";
             // 
-            // label3
+            // labelIdade
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(577, 182);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 15);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "label3";
+            this.labelIdade.AutoSize = true;
+            this.labelIdade.Location = new System.Drawing.Point(639, 135);
+            this.labelIdade.Name = "labelIdade";
+            this.labelIdade.Size = new System.Drawing.Size(36, 15);
+            this.labelIdade.TabIndex = 6;
+            this.labelIdade.Text = "Idade";
             // 
-            // label4
+            // labelGenero
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(581, 245);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 15);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "label4";
+            this.labelGenero.AutoSize = true;
+            this.labelGenero.Location = new System.Drawing.Point(639, 179);
+            this.labelGenero.Name = "labelGenero";
+            this.labelGenero.Size = new System.Drawing.Size(45, 15);
+            this.labelGenero.TabIndex = 7;
+            this.labelGenero.Text = "Genero";
             // 
-            // label5
+            // labelInicioLideranca
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(573, 296);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(38, 15);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "label5";
+            this.labelInicioLideranca.AutoSize = true;
+            this.labelInicioLideranca.Location = new System.Drawing.Point(639, 223);
+            this.labelInicioLideranca.Name = "labelInicioLideranca";
+            this.labelInicioLideranca.Size = new System.Drawing.Size(106, 15);
+            this.labelInicioLideranca.TabIndex = 8;
+            this.labelInicioLideranca.Text = "Início da Liderança";
             // 
-            // label6
+            // labelFimLideranca
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(500, 411);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(38, 15);
-            this.label6.TabIndex = 9;
-            this.label6.Text = "label6";
+            this.labelFimLideranca.AutoSize = true;
+            this.labelFimLideranca.Location = new System.Drawing.Point(639, 267);
+            this.labelFimLideranca.Name = "labelFimLideranca";
+            this.labelFimLideranca.Size = new System.Drawing.Size(97, 15);
+            this.labelFimLideranca.TabIndex = 9;
+            this.labelFimLideranca.Text = "Fim da Liderança";
             // 
-            // label7
+            // labelStatus
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(497, 475);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(38, 15);
-            this.label7.TabIndex = 10;
-            this.label7.Text = "label7";
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(640, 311);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(39, 15);
+            this.labelStatus.TabIndex = 10;
+            this.labelStatus.Text = "Status";
             // 
-            // label8
+            // labelElementoChakra
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(389, 469);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(38, 15);
-            this.label8.TabIndex = 11;
-            this.label8.Text = "label8";
+            this.labelElementoChakra.AutoSize = true;
+            this.labelElementoChakra.Location = new System.Drawing.Point(639, 363);
+            this.labelElementoChakra.Name = "labelElementoChakra";
+            this.labelElementoChakra.Size = new System.Drawing.Size(97, 15);
+            this.labelElementoChakra.TabIndex = 11;
+            this.labelElementoChakra.Text = "Elemento Chakra";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(640, 66);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(302, 23);
+            this.textBoxNome.TabIndex = 12;
+            // 
+            // comboBoxTitulo
+            // 
+            this.comboBoxTitulo.FormattingEnabled = true;
+            this.comboBoxTitulo.Location = new System.Drawing.Point(640, 110);
+            this.comboBoxTitulo.Name = "comboBoxTitulo";
+            this.comboBoxTitulo.Size = new System.Drawing.Size(302, 23);
+            this.comboBoxTitulo.TabIndex = 13;
+            // 
+            // textBoxIdade
+            // 
+            this.textBoxIdade.Location = new System.Drawing.Point(640, 154);
+            this.textBoxIdade.Name = "textBoxIdade";
+            this.textBoxIdade.Size = new System.Drawing.Size(301, 23);
+            this.textBoxIdade.TabIndex = 14;
+            // 
+            // comboBoxGenero
+            // 
+            this.comboBoxGenero.FormattingEnabled = true;
+            this.comboBoxGenero.Location = new System.Drawing.Point(640, 198);
+            this.comboBoxGenero.Name = "comboBoxGenero";
+            this.comboBoxGenero.Size = new System.Drawing.Size(302, 23);
+            this.comboBoxGenero.TabIndex = 15;
+            // 
+            // dateTimePickerInicioLideranca
+            // 
+            this.dateTimePickerInicioLideranca.Location = new System.Drawing.Point(640, 242);
+            this.dateTimePickerInicioLideranca.Name = "dateTimePickerInicioLideranca";
+            this.dateTimePickerInicioLideranca.Size = new System.Drawing.Size(302, 23);
+            this.dateTimePickerInicioLideranca.TabIndex = 16;
+            // 
+            // dateTimePickerFimLideranca
+            // 
+            this.dateTimePickerFimLideranca.Location = new System.Drawing.Point(640, 286);
+            this.dateTimePickerFimLideranca.Name = "dateTimePickerFimLideranca";
+            this.dateTimePickerFimLideranca.Size = new System.Drawing.Size(302, 23);
+            this.dateTimePickerFimLideranca.TabIndex = 17;
             // 
             // LiderForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 566);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(953, 637);
+            this.Controls.Add(this.dateTimePickerFimLideranca);
+            this.Controls.Add(this.dateTimePickerInicioLideranca);
+            this.Controls.Add(this.comboBoxGenero);
+            this.Controls.Add(this.textBoxIdade);
+            this.Controls.Add(this.comboBoxTitulo);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.labelElementoChakra);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.labelFimLideranca);
+            this.Controls.Add(this.labelInicioLideranca);
+            this.Controls.Add(this.labelGenero);
+            this.Controls.Add(this.labelIdade);
+            this.Controls.Add(this.labelTitulo);
+            this.Controls.Add(this.labelNome);
+            this.Controls.Add(this.buttonApagar);
+            this.Controls.Add(this.buttonEditar);
+            this.Controls.Add(this.buttonSalvar);
             this.Controls.Add(this.dataGridView1);
             this.Name = "LiderForm";
             this.Text = "LiderForm";
@@ -181,16 +237,22 @@
         #endregion
 
         private DataGridView dataGridView1;
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private Label label5;
-        private Label label6;
-        private Label label7;
-        private Label label8;
+        private Button buttonSalvar;
+        private Button buttonEditar;
+        private Button buttonApagar;
+        private Label labelNome;
+        private Label labelTitulo;
+        private Label labelIdade;
+        private Label labelGenero;
+        private Label labelInicioLideranca;
+        private Label labelFimLideranca;
+        private Label labelStatus;
+        private Label labelElementoChakra;
+        private TextBox textBoxNome;
+        private ComboBox comboBoxTitulo;
+        private TextBox textBoxIdade;
+        private ComboBox comboBoxGenero;
+        private DateTimePicker dateTimePickerInicioLideranca;
+        private DateTimePicker dateTimePickerFimLideranca;
     }
 }
