c0ba8aa0562cbf27013fc952c86531f8f754b80f
claudio raimundo <claudio.h.raimundo123@gmail.com>
2022-06-26 10:53:36 -0300

    Criação da classe Lider

--- Entra21-trabalho-02/Entra21-trabalho-02/Lider/Lider.cs
+++ Entra21-trabalho-02/Entra21-trabalho-02/Lider/Lider.cs
@@ -2,5 +2,13 @@
 {
     internal class Lider
     {
+        public int Codigo, Idade;
+        public string Nome;
+        public string Titulo;
+        public string Genero;
+        public DateTime InicioLideranca;
+        public DateTime FimLideranca;
+        public string Status;
+        public bool KekkeiGenkai;
     }
 }
