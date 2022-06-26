c22dd34ca6223db5998513104263a8b45efc6304
dbs07x <dbs07x@gmail.com>
2022-06-26 03:12:24 -0300

    Criação classe claform

---
+++ Entra21-trabalho-02/Entra21-trabalho-02/Clas/ClaForm.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Entra21_trabalho_02.Clas
+{
+    partial class ClaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNome = new System.Windows.Forms.Label();
+            this.buttonCadastrar = new System.Windows.Forms.Button();
+            this.buttonApagar = new System.Windows.Forms.Button();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.buttonEditar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelLider = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelKekkeiGenkai = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.radioButtonSim = new System.Windows.Forms.RadioButton();
+            this.radioButtonNao = new System.Windows.Forms.RadioButton();
+            this.ColumnCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnLider = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnKekkeiGenkai = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(12, 9);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(40, 15);
+            this.labelNome.TabIndex = 0;
+            this.labelNome.Text = "Nome";
+            // 
+            // buttonCadastrar
+            // 
+            this.buttonCadastrar.Location = new System.Drawing.Point(12, 157);
+            this.buttonCadastrar.Name = "buttonCadastrar";
+            this.buttonCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCadastrar.TabIndex = 1;
+            this.buttonCadastrar.Text = "Cadastrar";
+            this.buttonCadastrar.UseVisualStyleBackColor = true;
+            // 
+            // buttonApagar
+            // 
+            this.buttonApagar.Location = new System.Drawing.Point(12, 186);
+            this.buttonApagar.Name = "buttonApagar";
+            this.buttonApagar.Size = new System.Drawing.Size(75, 23);
+            this.buttonApagar.TabIndex = 2;
+            this.buttonApagar.Text = "Apagar";
+            this.buttonApagar.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Location = new System.Drawing.Point(93, 186);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancelar.TabIndex = 3;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            // 
+            // buttonEditar
+            // 
+            this.buttonEditar.Location = new System.Drawing.Point(93, 157);
+            this.buttonEditar.Name = "buttonEditar";
+            this.buttonEditar.Size = new System.Drawing.Size(75, 23);
+            this.buttonEditar.TabIndex = 4;
+            this.buttonEditar.Text = "Editar";
+            this.buttonEditar.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 5;
+            // 
+            // labelLider
+            // 
+            this.labelLider.AutoSize = true;
+            this.labelLider.Location = new System.Drawing.Point(12, 53);
+            this.labelLider.Name = "labelLider";
+            this.labelLider.Size = new System.Drawing.Size(33, 15);
+            this.labelLider.TabIndex = 6;
+            this.labelLider.Text = "Líder";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 71);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // labelKekkeiGenkai
+            // 
+            this.labelKekkeiGenkai.AutoSize = true;
+            this.labelKekkeiGenkai.Location = new System.Drawing.Point(12, 97);
+            this.labelKekkeiGenkai.Name = "labelKekkeiGenkai";
+            this.labelKekkeiGenkai.Size = new System.Drawing.Size(80, 15);
+            this.labelKekkeiGenkai.TabIndex = 8;
+            this.labelKekkeiGenkai.Text = "Kekkei Genkai";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnCodigo,
+            this.ColumnNome,
+            this.ColumnLider,
+            this.ColumnKekkeiGenkai});
+            this.dataGridView1.Location = new System.Drawing.Point(202, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(586, 237);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // radioButtonSim
+            // 
+            this.radioButtonSim.AutoSize = true;
+            this.radioButtonSim.Location = new System.Drawing.Point(12, 115);
+            this.radioButtonSim.Name = "radioButtonSim";
+            this.radioButtonSim.Size = new System.Drawing.Size(45, 19);
+            this.radioButtonSim.TabIndex = 10;
+            this.radioButtonSim.TabStop = true;
+            this.radioButtonSim.Text = "Sim";
+            this.radioButtonSim.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonNao
+            // 
+            this.radioButtonNao.AutoSize = true;
+            this.radioButtonNao.Location = new System.Drawing.Point(65, 115);
+            this.radioButtonNao.Name = "radioButtonNao";
+            this.radioButtonNao.Size = new System.Drawing.Size(47, 19);
+            this.radioButtonNao.TabIndex = 11;
+            this.radioButtonNao.TabStop = true;
+            this.radioButtonNao.Text = "Não";
+            this.radioButtonNao.UseVisualStyleBackColor = true;
+            // 
+            // ColumnCodigo
+            // 
+            this.ColumnCodigo.HeaderText = "Código";
+            this.ColumnCodigo.Name = "ColumnCodigo";
+            this.ColumnCodigo.ReadOnly = true;
+            this.ColumnCodigo.Visible = false;
+            // 
+            // ColumnNome
+            // 
+            this.ColumnNome.HeaderText = "Nome";
+            this.ColumnNome.Name = "ColumnNome";
+            this.ColumnNome.ReadOnly = true;
+            // 
+            // ColumnLider
+            // 
+            this.ColumnLider.HeaderText = "Líder";
+            this.ColumnLider.Name = "ColumnLider";
+            this.ColumnLider.ReadOnly = true;
+            // 
+            // ColumnKekkeiGenkai
+            // 
+            this.ColumnKekkeiGenkai.HeaderText = "Kekkei Genkai";
+            this.ColumnKekkeiGenkai.Name = "ColumnKekkeiGenkai";
+            this.ColumnKekkeiGenkai.ReadOnly = true;
+            // 
+            // ClaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 262);
+            this.Controls.Add(this.radioButtonNao);
+            this.Controls.Add(this.radioButtonSim);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.labelKekkeiGenkai);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labelLider);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonEditar);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.buttonApagar);
+            this.Controls.Add(this.buttonCadastrar);
+            this.Controls.Add(this.labelNome);
+            this.Name = "ClaForm";
+            this.Text = "ClaForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label labelNome;
+        private Button buttonCadastrar;
+        private Button buttonApagar;
+        private Button buttonCancelar;
+        private Button buttonEditar;
+        private TextBox textBox1;
+        private Label labelLider;
+        private ComboBox comboBox1;
+        private Label labelKekkeiGenkai;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn ColumnCodigo;
+        private DataGridViewTextBoxColumn ColumnNome;
+        private DataGridViewTextBoxColumn ColumnLider;
+        private DataGridViewTextBoxColumn ColumnKekkeiGenkai;
+        private RadioButton radioButtonSim;
+        private RadioButton radioButtonNao;
+    }
+}
